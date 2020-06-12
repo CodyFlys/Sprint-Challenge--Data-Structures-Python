7cbb807adeeb8ecbf0cc066903790555d36fe900
Cody <codysolomon15@gmail.com>
2020-06-12 10:32:23 -0700

    completed sprint mvp maybe stretch?

--- names/names.py
+++ names/names.py
@@ -12,11 +12,49 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None: 
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value) 
+            else:
+                self.right.insert(value)
+                
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        # Check if target == self.value
+        if target == self.value:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            return self.right.contains(target)
+
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+root_node = BSTNode(names_1[0])
+for name in names_1:
+    # for every name in names_1 we insert the name as a node.
+    root_node.insert(name)
+for name2 in names_2:
+    # for ever name in names_2 we check if the root_node contains name2, if so we put it in duplicates by appending it to the list.
+    if root_node.contains(name2):
+        duplicates.append(name2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
